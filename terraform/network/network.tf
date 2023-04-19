d5a53952b5f3d53c96c7eecfb10b3e539234e240
mango <mchead1@naver.com>
2023-04-19 17:00:57 +0900

    chore: test planning add subnets(public / private)

--- terraform/network/network.tf
+++ terraform/network/network.tf
@@ -3,3 +3,28 @@ resource "aws_vpc" "main" {
   enable_dns_support    = true
   enable_dns_hostnames  = true
 }
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "private" {
+  vpc_id            = aws_vpc.main.id
+  count             = 3
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+
+  tags = {
+    Name = "ecs-private-subnet"
+  }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  count                   = 3
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, 3 + count.index)
+  availability_zone       = element(data.aws_availability_zones.available.names, count.index)
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "ecs-public-subnet"
+  }
+}
